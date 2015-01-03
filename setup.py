84d205b97ca6240608be92b7a51339340062e056
Andrew Young <andrewyoung1991@gmail.com>
2015-01-02 16:59:06 -0800

    adding setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+setup(
+    name='django-under-maintenance',
+    packages=find_packages(),
+    version=version,
+    description='store django request objects for debugging server errors.',
+    author='Andrew Young',
+    author_email='ayoung@thewulf.org',
+    download_url='https://github.com/andrewyoung1991/django-error-store.git',
+    keywords=['django', 'debug', 'request', 'server-error'],
+    package_data={},
+)
